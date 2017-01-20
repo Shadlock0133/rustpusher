c7ac2c73ad59edf40aa610e0282fb4893910723e
Shadlock0133 <tomek-kubel2@wp.pl>
2017-01-20 17:58:05 +0100

    Refactor minifb out of lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,7 @@
 #![feature(box_syntax)]
-extern crate minifb;
-
 use std::fs::File;
 use std::io::Read;
 use std::ops::DerefMut;
-use minifb::{Key, Window, WindowOptions};
 
 const PAGE: usize = 0x100;
 const BANK: usize = PAGE * PAGE;
@@ -12,72 +9,24 @@ const MEMORY: usize = BANK * PAGE + 8;
 
 pub struct BytePusher {
     pub ram: Box<[u8; MEMORY]>,
-    pub window: Window,
+    pub step_counter: u16,
 }
 
 impl BytePusher {
-    fn read_file(file: &str) -> Box<[u8; MEMORY]> {
-        let mut file = File::open(file).expect("Unable to open file.");
-        let mut buf = box [0u8; MEMORY];
-        file.read(buf.deref_mut()).expect("Unable to read file.");
-        buf
-    }
-
     pub fn new(file: &str) -> BytePusher {
-        let ram = BytePusher::read_file(file);
-
-        let window = Window::new("BytePusher Test",
-                                    PAGE,
-                                    PAGE,
-                                    WindowOptions::default())
-                                    .expect("Unable to create window");
-
         BytePusher {
-            ram: ram,
-            window: window,
+            ram: BytePusher::read_file(file),
+            step_counter: 0,
         }
     }
 
-    pub fn process_input(&mut self) {
-        let mut keyboard = 0u16;
-        self.window.get_keys().map(|keys|
-            for k in keys {
-                match k {
-                    Key::X    => keyboard |= 0b0000000000000001,
-
-                    Key::Key1 => keyboard |= 0b0000000000000010,
-                    Key::Key2 => keyboard |= 0b0000000000000100,
-                    Key::Key3 => keyboard |= 0b0000000000001000,
-
-                    Key::Q    => keyboard |= 0b0000000000010000,
-                    Key::W    => keyboard |= 0b0000000000100000,
-                    Key::E    => keyboard |= 0b0000000001000000,
-
-                    Key::A    => keyboard |= 0b0000000010000000,
-                    Key::S    => keyboard |= 0b0000000100000000,
-                    Key::D    => keyboard |= 0b0000001000000000,
-
-                    Key::Z    => keyboard |= 0b0000010000000000,
-                    Key::C    => keyboard |= 0b0000100000000000,
-
-                    Key::Key4 => keyboard |= 0b0001000000000000,
-                    Key::R    => keyboard |= 0b0010000000000000,
-                    Key::F    => keyboard |= 0b0100000000000000,
-                    Key::V    => keyboard |= 0b1000000000000000,
-
-                    _ => {}
-                }
-            }
-        );
-        self.ram[0] = (keyboard / PAGE as u16) as u8;
-        self.ram[1] = (keyboard % PAGE as u16) as u8;
+    pub fn process_input(&mut self, input: (u8, u8)) {
+        self.ram[0] = input.0;
+        self.ram[1] = input.1;
     }
 
-    fn address_at(&self, offset: usize) -> usize {
-        self.ram[offset    ] as usize * BANK +
-        self.ram[offset + 1] as usize * PAGE +
-        self.ram[offset + 2] as usize
-    }
+    // TODO: properly refactor for debugger use
+    // pub fn step(&mut self) {}
 
     pub fn frame(&mut self) {
         let mut pc = self.address_at(2);
@@ -90,37 +39,25 @@ impl BytePusher {
         }
     }
 
-    fn color_from_palette(index: u8) -> u32 {
-        match index {
-            0x00...0xd7 =>  index as u32 / 36    * 0x33 * BANK as u32 +
-                            index as u32 / 6 % 6 * 0x33 * PAGE as u32 +
-                            index as u32     % 6 * 0x33,
-            _ => 0x000000
-        }
-    }
-
-    pub fn update_window(&mut self) {
-        let mut window_buffer: Vec<u32> = vec![0; PAGE * PAGE];
-
+    pub fn get_screen_slice(&self) -> &[u8] {
         let offset = self.ram[5] as usize * BANK;
-
-        for (i, pixel) in window_buffer.iter_mut().enumerate() {
-            *pixel = BytePusher::color_from_palette(self.ram[offset + i]);
-        }
-
-        self.window.update_with_buffer(&window_buffer);
+        &self.ram[offset..offset + BANK]
     }
 
-    fn audio(&self) {
-        let start = self.ram[6] as usize * BANK + self.ram[7] as usize * PAGE;
-        let end = start + PAGE;
-        let buf = &self.ram[start..end];
+    pub fn get_audio_slice(&self) -> &[u8] {
+        let offset = self.ram[6] as usize * BANK + self.ram[7] as usize * PAGE;
+        &self.ram[offset..offset + PAGE]
     }
 
-    pub fn update(&mut self) {
-        self.process_input();
-        self.frame();
-        self.update_window();
-        self.audio();
+    fn read_file(file: &str) -> Box<[u8; MEMORY]> {
+        let mut file = File::open(file).expect("Unable to open file.");
+        let mut buf = box [0u8; MEMORY];
+        file.read(buf.deref_mut()).expect("Unable to read file.");
+        buf
+    }
+
+    fn address_at(&self, offset: usize) -> usize {
+        self.ram[offset] as usize * BANK + self.ram[offset + 1] as usize * PAGE +
+        self.ram[offset + 2] as usize
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,80 @@ extern crate rustpusher;
 use std::env::args;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
-use minifb::Key;
+use minifb::*;
 use rustpusher::*;
+
+const PAGE: usize = 0x100;
+const BANK: usize = PAGE * PAGE;
 
 fn main() {
     let file = args().nth(1).expect("No filename given.");
     let mut emu = BytePusher::new(&file);
-    emu.update_window();
+    let mut window = Window::new("BytePusher Test", PAGE, PAGE, WindowOptions::default())
+        .expect("Unable to create window");
+    let mut window_buffer: Vec<u32> = vec![0; PAGE * PAGE];
 
     loop {
-        if !emu.window.is_open() || emu.window.is_key_down(Key::Escape) {
+        if !window.is_open() || window.is_key_down(Key::Escape) {
             break;
         }
         let timer = Instant::now();
-        emu.update();
+
+        emu.process_input(get_input(&window));
+        emu.frame();
+
+        // TODO: properly refactor
+        let offset = emu.ram[5] as usize * BANK;
+        for (i, pixel) in window_buffer.iter_mut().enumerate() {
+            *pixel = color_from_palette(emu.ram[offset + i]);
+        }
+
+        window.update_with_buffer(&window_buffer);
+
         if let Some(value) = Duration::new(0, 16666666).checked_sub(timer.elapsed()) {
             sleep(value);
         }
+    }
+}
+
+fn get_input(ref window: &Window) -> (u8, u8) {
+    let mut input = (0u8, 0u8);
+    window.get_keys().map(|keys| for k in keys {
+        match k {
+            Key::X => input.1 |= 0b00000001,
+
+            Key::Key1 => input.1 |= 0b00000010,
+            Key::Key2 => input.1 |= 0b00000100,
+            Key::Key3 => input.1 |= 0b00001000,
+
+            Key::Q => input.1 |= 0b00010000,
+            Key::W => input.1 |= 0b00100000,
+            Key::E => input.1 |= 0b01000000,
+
+            Key::A => input.1 |= 0b10000000,
+            Key::S => input.0 |= 0b00000001,
+            Key::D => input.0 |= 0b00000010,
+
+            Key::Z => input.0 |= 0b00000100,
+            Key::C => input.0 |= 0b00001000,
+
+            Key::Key4 => input.0 |= 0b00010000,
+            Key::R => input.0 |= 0b00100000,
+            Key::F => input.0 |= 0b01000000,
+            Key::V => input.0 |= 0b10000000,
+
+            _ => {}
+        }
+    });
+    input
+}
+
+fn color_from_palette(index: u8) -> u32 {
+    match index {
+        0x00...0xd7 => {
+            index as u32 / 36 * 0x33 * BANK as u32 + index as u32 / 6 % 6 * 0x33 * PAGE as u32 +
+            index as u32 % 6 * 0x33
+        }
+        _ => 0x000000,
     }
 }
