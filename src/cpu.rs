d4089258c8821bceb54184398a16e7b3f0193d91
Shadlock0133 <tomek-kubel2@wp.pl>
2017-09-10 19:42:54 +0200

    first pass on libretro core impl

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,52 +1,61 @@
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read, Write};
+
+use errors::*;
 
 pub const PAGE: usize = 0x100;
 pub const BANK: usize = PAGE * PAGE;
 pub const MEMORY: usize = BANK * PAGE;
 pub const FULL_MEMORY: usize = MEMORY + 8;
+
 pub const KEYBOARD: usize = 0;
 pub const PC: usize = 2;
 pub const VIDEO: usize = 5;
 pub const AUDIO: usize = 6;
 
 pub struct Cpu {
-    pub ram: Box<[u8; FULL_MEMORY]>,
-    pub pc: u32,
-    pub step_counter: u32,
+    memory: Box<[u8; FULL_MEMORY]>,
+    pc: u32,
+    step_counter: u32,
 }
 
 impl Cpu {
     pub fn new() -> Self {
         Self {
-            ram: box [0; FULL_MEMORY],
+            memory: box [0; FULL_MEMORY],
             pc: 0,
             step_counter: 0,
         }
     }
 
-    #[allow(unknown_lints)]
-    #[allow(unused_io_amount)]
-    pub fn load_file(&mut self, file: &str) {
-        let mut file = File::open(file).unwrap();
-        file.read(&mut self.ram[..MEMORY]).expect("Unable to read file.");
-        for x in 0..8 {
-            assert_eq!(self.ram[MEMORY + x], 0);
-        }
+    pub fn load_file(&mut self, file: &str) -> Result<()> {
+        let mut file = File::open(file)?;
+        file.read(&mut self.memory[..MEMORY])?;
+        Ok(())
+    }
+
+    pub fn load_data(&mut self, data: &[u8]) -> Result<()> {
+        (&mut self.memory[..MEMORY]).write(data)?;
+        Ok(())
+    }
+
+    pub fn clear_memory(&mut self) {
+        self.memory = box [0; FULL_MEMORY]
     }
 
     pub fn process_input(&mut self, input: (u8, u8)) {
-        self.ram[KEYBOARD] = input.0;
-        self.ram[KEYBOARD + 1] = input.1;
+        self.memory[KEYBOARD] = input.0;
+        self.memory[KEYBOARD + 1] = input.1;
     }
 
-    pub fn step(&mut self) {
+    #[inline]
+    fn step(&mut self) {
         self.step_counter += 1;
         let pc = self.pc as usize;
         let src = self.address_at(pc);
-        let byte = self.ram[src];
+        let byte = self.memory[src];
         let dst = self.address_at(pc + 3);
-        self.ram[dst] = byte;
+        self.memory[dst] = byte;
         self.pc = self.address_at(pc + 6) as u32;
     }
 
@@ -59,18 +68,18 @@ impl Cpu {
     }
 
     pub fn get_video_slice(&self) -> &[u8] {
-        let offset = self.ram[VIDEO] as usize * BANK;
-        &self.ram[offset..offset + BANK]
+        let offset = self.memory[VIDEO] as usize * BANK;
+        &self.memory[offset..offset + BANK]
     }
 
     pub fn get_audio_slice(&self) -> &[u8] {
-        let offset = self.ram[AUDIO] as usize * BANK + self.ram[AUDIO + 1] as usize * PAGE;
-        &self.ram[offset..offset + PAGE]
+        let offset = self.memory[AUDIO] as usize * BANK + self.memory[AUDIO + 1] as usize * PAGE;
+        &self.memory[offset..offset + PAGE]
     }
 
     fn address_at(&self, offset: usize) -> usize {
-        self.ram[offset] as usize * BANK +
-        self.ram[offset + 1] as usize * PAGE +
-        self.ram[offset + 2] as usize
+        self.memory[offset] as usize * BANK +
+        self.memory[offset + 1] as usize * PAGE +
+        self.memory[offset + 2] as usize
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,124 +1,159 @@
 #![feature(box_syntax)]
-extern crate minifb;
-extern crate hound;
+#[macro_use] extern crate libretro_backend;
+#[macro_use] extern crate error_chain;
+
+use libretro_backend::*;
 
 mod cpu;
-
 use cpu::*;
-use minifb::{Key, Scale, Window, WindowOptions};
-use hound::WavWriter;
-use std::fs::File;
-use std::io::BufWriter;
-use std::thread;
-use std::time::{Duration, Instant};
 
-pub struct Emu {
+mod errors {
+    error_chain!{
+        foreign_links {
+            Io(::std::io::Error);
+        }
+    }
+}
+
+pub struct RPCore {
     cpu: Cpu,
-    window: Window,
-    audio_output: Option<WavWriter<BufWriter<File>>>,
-    turbo: bool,
-    paused: bool,
+    palette: [u32; 256],
+    video_buffer: [u32; BANK],
+    audio_buffer: Vec<i16>,
+    game_data: Option<GameData>,
 }
 
-impl Emu {
-    pub fn from_file(rom_file: &str, wav_file: Option<&str>) -> Self {
-        let mut cpu = Cpu::new();
-        cpu.load_file(rom_file);
-
-        let win_options = WindowOptions { scale: Scale::X2, ..WindowOptions::default() };
-        let window = Window::new("Rustpusher", PAGE, PAGE, win_options)
-        .expect("Unable to create window.");
-
-        let audio_spec = hound::WavSpec {
-            channels: 2,
-            sample_rate: 15360,
-            bits_per_sample: 8,
-            sample_format: hound::SampleFormat::Int
-        };
-        let audio_output = wav_file.map(|wav_file| WavWriter::create(wav_file, audio_spec).unwrap());
-
-        Emu {
-            cpu, window, audio_output,
-            turbo: false,
-            paused: false,
+impl RPCore {
+    fn new() -> Self {
+        RPCore {
+            cpu: Cpu::new(),
+            palette: Self::default_palette(),
+            video_buffer: [0; BANK],
+            audio_buffer: Vec::with_capacity(15360),
+            game_data: None,
         }
     }
 
-    pub fn run(&mut self) {
-        while self.window.is_open() && !self.window.is_key_down(Key::Escape) {
-            let timer = Instant::now();
-
-            let input = self.get_input();
-            self.cpu.process_input(input);
-            if !self.paused {
-                self.cpu.finish_frame();
-            }
-
-            let window_buffer: Vec<u32> = self.cpu
-                .get_video_slice()
-                .iter()
-                .map(|&x| Self::color_from_palette(x))
-                .collect();
-            self.window.update_with_buffer(&window_buffer);
-
-            if let Some(ref mut writer) = self.audio_output {
-                for sample in self.cpu.get_audio_slice() {
-                    writer.write_sample(*sample as i8).unwrap();
-                    writer.write_sample(*sample as i8).unwrap();
+    fn default_palette() -> [u32; 256] {
+        let mut palette = [0; 256];
+        for (index, out) in palette.iter_mut().enumerate() {
+            *out = match index {
+                0x00...0xd7 => {
+                    index as u32 / 36 * 0x33 * BANK as u32 +
+                    index as u32 / 6 % 6 * 0x33 * PAGE as u32 +
+                    index as u32 % 6 * 0x33
                 }
-            }
-
-            if !self.turbo {
-                if let Some(value) = Duration::new(0, 1_000_000_000 / 60).checked_sub(timer.elapsed()) {
-                    thread::sleep(value);
-                }
+                _ => 0x000000,
             }
         }
+        palette
+    }
+}
+
+impl Default for RPCore {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Core for RPCore {
+    fn info() -> CoreInfo {
+        CoreInfo::new("Rustpusher", env!("CARGO_PKG_VERSION"))
+            .supports_roms_with_extension(".BytePusher")
     }
 
-    fn color_from_palette(index: u8) -> u32 {
-        match index {
-            0x00...0xd7 => {
-                index as u32 / 36 * 0x33 * BANK as u32 +
-                index as u32 / 6 % 6 * 0x33 * PAGE as u32 +
-                index as u32 % 6 * 0x33
-            }
-            _ => 0x000000,
+    fn on_load_game(&mut self, game_data: GameData) -> LoadGameResult {
+        if game_data.is_empty() {
+            return LoadGameResult::Failed(game_data);
         }
+        
+        match if let Some(data) = game_data.data() {
+            self.cpu.load_data(data)
+        } else if let Some(path) = game_data.path() {
+            self.cpu.load_file(path)
+        } else {
+            panic!("Unable to load game data")
+        } {
+            Ok(_) => {
+                self.game_data = Some(game_data);
+                LoadGameResult::Success(
+                    AudioVideoInfo::new()
+                        .video(256, 256, 60.0, PixelFormat::ARGB8888)
+                        .audio(15360.0)
+                )
+            }
+            Err(_) => LoadGameResult::Failed(game_data)
+        }
+
     }
 
-    fn get_input(&mut self) -> (u8, u8) {
+    fn on_unload_game(&mut self) -> GameData {
+        self.cpu.clear_memory();
+        self.game_data.take().unwrap()
+    }
+
+    fn on_run(&mut self, handle: &mut RuntimeHandle) {
+        macro_rules! update_input {
+            ( $( $button:ident => $expr:expr ,)* ) => (
+                use JoypadButton::*;
+                $( if handle.is_joypad_button_pressed(0, $button) { $expr } )*
+            )
+        }
         let mut input = (0u8, 0u8);
-        self.window.get_keys().map(|keys| for k in keys {
-            match k {
-                Key::T => self.turbo = !self.turbo,
-                Key::P => self.paused = !self.paused,
+        update_input!(
+            Up     => input.1 |= 0x1,
+            Down   => input.1 |= 0x2,
+            Left   => input.1 |= 0x4,
+            Right  => input.1 |= 0x8,
+            A      => input.1 |= 0x10,
+            B      => input.1 |= 0x20,
+            X      => input.1 |= 0x40,
+            Y      => input.1 |= 0x80,
+            Select => input.0 |= 0x1,
+            Start  => input.0 |= 0x2,
+            L1     => input.0 |= 0x4,
+            L2     => input.0 |= 0x8,
+            L3     => input.0 |= 0x10,
+            R1     => input.0 |= 0x20,
+            R2     => input.0 |= 0x40,
+            R3     => input.0 |= 0x80,
+        );
+        self.cpu.process_input(input);
 
-                Key::X => input.1 |= 0b00000001,
+        self.cpu.finish_frame();
 
-                Key::Key1 => input.1 |= 0b00000010,
-                Key::Key2 => input.1 |= 0b00000100,
-                Key::Key3 => input.1 |= 0b00001000,
+        for (&pixel, out) in self.cpu.get_video_slice().iter().zip(self.video_buffer.iter_mut()) {
+            *out = self.palette[pixel as usize];
+        }
+        handle.upload_video_frame(as_bytes(&self.video_buffer));
 
-                Key::Q => input.1 |= 0b00010000,
-                Key::W => input.1 |= 0b00100000,
-                Key::E => input.1 |= 0b01000000,
+        for &x in self.cpu.get_audio_slice().iter() {
+            let x = (x as i16) << 8;
+            self.audio_buffer.push(x);
+            self.audio_buffer.push(x);
+        }
+        handle.upload_audio_frame(&self.audio_buffer[..]);
+        self.audio_buffer.clear();
+    }
 
-                Key::A => input.1 |= 0b10000000,
-                Key::S => input.0 |= 0b00000001,
-                Key::D => input.0 |= 0b00000010,
-
-                Key::Z => input.0 |= 0b00000100,
-                Key::C => input.0 |= 0b00001000,
-
-                Key::Key4 => input.0 |= 0b00010000,
-                Key::R => input.0 |= 0b00100000,
-                Key::F => input.0 |= 0b01000000,
-                Key::V => input.0 |= 0b10000000,
-
-                _ => {}
+    fn on_reset(&mut self) {
+        if let Some(ref game_data) = self.game_data {
+            if let Some(data) = game_data.data() {
+                let _ = self.cpu.load_data(data);
+            } else if let Some(path) = game_data.path() {
+                let _ = self.cpu.load_file(path);
             }
-        });
-        input
+        }
     }
 }
+
+#[inline]
+pub fn as_bytes<T: Copy>(array: &[T]) -> &[u8] {
+    unsafe {
+        std::slice::from_raw_parts(
+            std::mem::transmute(array.as_ptr()), 
+            std::mem::size_of::<T>() * array.len())
+    }
+}
+
+libretro_core!(RPCore);
